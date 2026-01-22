259bce79de7d661e479d57e7f97c81affffd117a
returntohealthtw-maker <returntohealth.tw@gmail.com>
2026-01-22 08:43:09 +0800

    Add missing Header component

---
+++ BrainwaveEducation/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header style={{ padding: "12px 16px", borderBottom: "1px solid #ddd" }}>
+      <h1 style={{ margin: 0, fontSize: 18 }}>Brainwave Education</h1>
+    </header>
+  );
+}
